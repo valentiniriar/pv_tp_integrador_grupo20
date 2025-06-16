28585e6e2f2828078141a760073759050ab0f311
MauroNanda <39233689@fi.unju.edu.ar>
2025-06-16 01:01:55 -0300

    feat: Mejoras UX/UI y sistema de notificaciones

--- pv_tp_integrador_grupo20/src/main.jsx
+++ pv_tp_integrador_grupo20/src/main.jsx
@@ -4,9 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ProductProvider } from "./context/ProductContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import { ToastProvider } from "./context/ToastContext";
 import "./styles/navbar.css";
 import "./styles/loading.css";
 import "./styles/error.css";
+import "./styles/toast.css";
+import "./styles/product-card.css";
+import "./styles/transitions.css";
+import "./styles/buttons.css";
 import "./index.css";
 
 // ** LA IMPORTACIÓN CRÍTICA QUE CARGA TODOS LOS ESTILOS **
@@ -17,7 +22,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <ThemeProvider>
         <ProductProvider>
-          <App />
+          <ToastProvider>
+            <App />
+          </ToastProvider>
         </ProductProvider>
       </ThemeProvider>
     </BrowserRouter>
